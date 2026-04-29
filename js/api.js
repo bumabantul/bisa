742fadfe9159b2a1a2d19a2b78a33579e5d64222
bumabantul <bumabantul@gmail.com>
2026-04-29 08:52:46 +0700

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // GANTI DENGAN URL APPS SCRIPT ANDA
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyj8bsCAtp3Ht8c7tqeRA3Valf5qdf13GvZ-4kZwnuaXnRYfFPrGEHCawfNcijs_EezPQ/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbycusPlO5Y8uUqzU7gk6zwSTmxRiBMgZT9qoiYiaxqEeMN2fITXcdON4l45QvWKNqR9tQ/exec';
 
 /**
  * Memanggil API Apps Script menggunakan JSONP (untuk data kecil, backward compatibility)
